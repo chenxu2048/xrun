8c7b053788fa091077b204738dd57ba2e697d3a3
chenxu <chenxu2048@gmail.com>
2019-03-19 20:56:26 +0800

    参数解析

--- src/xrun/xrun.c
+++ src/xrun/xrun.c
@@ -17,11 +17,30 @@ static int retval = 0;
 static xr_option_t xrn_option = {};
 static xr_entry_t xrn_entry = {};
 
-static int nthread;
-static int nprocess;
-static int memory;
-static int nfile;
-static xr_time_t time;
+#define _XRN_ACCESS_ENTRY 2048
+#define _XRN_CAN_ADD_ENTRY(entry) (entry < _XRN_ACCESS_ENTRY)
+
+static char *dir_access[_XRN_ACCESS_ENTRY] = {};
+static size_t n_dir_access = 0;
+static char *file_access[_XRN_ACCESS_ENTRY] = {};
+static size_t n_file_access = 0;
+
+#define XRN_OPTION_ERROR(msg, ...)                                   \
+  do {                                                               \
+    char errbuf[256];                                                \
+    errbuf[0] = '\0';                                                \
+    size_t errsize =                                                 \
+      snprintf(errbuf, 256, "%s: " msg "\n", __func__, __VA_ARGS__); \
+    xr_string_concat_raw(&xrn_error, errbuf, errsize);               \
+  } while (0)
+
+static xr_string_t xrn_error;
+
+static long nthread;
+static long nprocess;
+static long memory;
+static long nfile;
+static xr_time_ms_t time;
 
 #define XR_FILE_LIMIT_SIZE 128
 
@@ -113,7 +132,17 @@ const static struct xrn_option options[] = {
   },
 };
 
-void xr_xrun_parse_opt(int argc, const char *argv[]) {
+#define _XRN_SET_GLOBAL_INT(var, val, option)                     \
+  do {                                                            \
+    char *endptr = NULL;                                          \
+    var = strtol(val, &endptr, 10);                               \
+    if (*endptr != '\0') {                                        \
+      retval = 1;                                                 \
+      XRN_OPTION_ERROR("invalid option %s with %s", option, val); \
+    }                                                             \
+  } while (0)
+
+void xrn_parse_opt(int argc, const char *argv[]) {
   int option_index = 0;
   opterr = 0;
   xr_string_t error;
@@ -125,23 +154,47 @@ void xr_xrun_parse_opt(int argc, const char *argv[]) {
         return;
       case 'c': {
         if (config_path != NULL) {
-          fputs("--config is not unique.\n", stderr);
+          retval = 1;
+          XRN_OPTION_ERROR("--config is not unique.\n");
         }
         config_path = optarg;
-        if (xrn_config_parse(config_path, option, error) == false) {
-          retval = 1;
-          return;
-        }
         break;
       }
       case 'C':
-      case 'd':
-      case 'f':
+      case 'd': {
+        if (_XRN_CAN_ADD_ENTRY(n_dir_access)) {
+          dir_access[n_dir_access++] = optarg;
+        } else {
+          retval = 1;
+          XRN_OPTION_ERROR(
+            "--directory/-d too many entries. using config file instead.");
+        }
+        break;
+      }
+      case 'f': {
+        if (_XRN_CAN_ADD_ENTRY(n_file_access)) {
+          file_access[n_file_access++] = optarg;
+        } else {
+          retval = 1;
+          XRN_OPTION_ERROR(
+            "--directory/-d too many entries. using config file instead.");
+        }
+        break;
+      }
       case 'F':
+        _XRN_SET_GLOBAL_INT(nprocess, optarg, "--fork/-F");
+        break;
       case 'm':
+        _XRN_SET_GLOBAL_INT(memory, optarg, "--memory/-m");
+        break;
       case 'n':
+        _XRN_SET_GLOBAL_INT(nfile, optarg, "--nfile/-n");
+        break;
       case 't':
+        _XRN_SET_GLOBAL_INT(time, optarg, "--time/-t");
+        break;
       case 'T':
+        _XRN_SET_GLOBAL_INT(nthread, optarg, "--thread/-T");
         break;
       case 'v':
         version = true;
@@ -154,9 +207,19 @@ void xr_xrun_parse_opt(int argc, const char *argv[]) {
       default:
         break;
     }
+    if (retval == 1) {
+      return;
+    }
   }
 }
 
 int main(int argc, const char *argv[]) {
-  xr_xrun_parse_opt(argc, argv, &error);
+  xrn_parse_opt(argc, argv);
+  if (help == true) {
+    xrn_print_options(&options);
+  }
+  if (retval == 1) {
+    fputs(error.string, stderr);
+  } else {
+  }
 }
